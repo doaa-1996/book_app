9099b470bd592ad4e8f106685a8983293e6517c9
doaa-1996 <doaaobeidat1996@gmail.com>
2021-05-03 01:58:22 +0300

    first edit

--- server.js
+++ server.js
@@ -21,28 +21,28 @@ server.get('/',(req,res)=>{
   console.log('you are in the main page');
 });
 
-server.get('/show',(req,res)=>{
-  res.render('./pages/searches/show');
-  console.log('you are in the main page !');
-//   console.log('search done');
+server.get('/test',(req,res)=>{
+
+  console.log('Hello , Welcome to book app! powered by Doaa obeidat ');
+
+
+
 });
 
 
 server.get('/search',(req,res)=>{
+  res.render('./pages/searches/show');
+  console.log('You are in the search page!');
+
+});
+
+
+
+server.get('/new',(req,res)=>{
   res.render('./pages/new');
   console.log('You are in the search page!');
 
 });
-
-
-server.get('/show',(req,res)=>{
-  res.render('./pages/show');
-  console.log('You are in the search page!');
-
-});
-
-
-
 
 
 server.post('/searches',(req,res)=>{
@@ -51,8 +51,9 @@ server.post('/searches',(req,res)=>{
   let url = `https://www.googleapis.com/books/v1/volumes?q=+${search_by}:${keyword}`;
   superagent.get(url)
     .then(booksdata => {
-      console.log(booksdata.body.items);
-      res.render('pages/searches/show',{booksArr:booksdata.body.items});
+      let result = booksdata.body.items.slice(0, 10);
+      let formattedResutl = result.map(bookData => new Book(bookData));
+      res.render('pages/searches/show', {books: formattedResutl});
     });
 
 });
@@ -60,4 +61,18 @@ server.post('/searches',(req,res)=>{
 
 
 
+
+
+server.get('*',(req,res)=>{
+
+  console.log('Sorry,something wrong');
+});
+
+
+function Book(data) {
+
+  this.title = data.volumeInfo.title;
+  this.auther = data.volumeInfo.authors ? data.volumeInfo.authors.join(', ') : 'Anonymous';
+  this.description = data.volumeInfo.title;
+}
 
