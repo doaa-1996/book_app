a0534e443c853738e304bcc88b299a3f7600057a
doaa-1996 <doaaobeidat1996@gmail.com>
2021-05-03 23:43:32 +0300

    edit lab11

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # book_app
 
-**Author**: Your Name Goes Here
+**Author**: Doa'a Obeidat
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for a Code 301 class. (i.e. What's your problem domain?) -->
+A full stack application allow you to search for a book using its name or its author and retrieve the book from Google Books API.
+
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+clone the repository using https://github.com/doaa-1996/book_app.git and install the required backage using npm i and then start the server using nodemon.
+
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+
+1. HTML,CSS and  node.js 
+2. Frameworks and libraries used: express, cors, ejs, dotenv, superagent, method-override
+3. PIs: Google Books API
+
 
 ## Change Log
-<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
 
-01-01-2001 4:59pm - Application now has a fully-functional express server, with GET and POST routes for the book resource.
 
-## Credits and Collaborations
-<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+02-05-2021 11:59pm - Application now has a fully-functional express server, with GET and POST routes for the book resource, and needs some CSS design.
+03-05-2021 11:00pm - Application Done .
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 const superagent = require('superagent');
 server.use(cors());
 
-server.set('view engine','ejs');
-server.use(express.urlencoded({extended:true}));
+server.set('view engine', 'ejs');
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static('./public'));
 
 
@@ -16,12 +16,12 @@ server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
-server.get('/',(req,res)=>{
+server.get('/', (req, res) => {
   res.render('./pages/index');
   console.log('you are in the main page');
 });
 
-server.get('/test',(req,res)=>{
+server.get('/test', (req, res) => {
 
   console.log('Hello , Welcome to book app! powered by Doaa obeidat ');
 
@@ -30,7 +30,7 @@ server.get('/test',(req,res)=>{
 });
 
 
-server.get('/search',(req,res)=>{
+server.get('/search', (req, res) => {
   res.render('./pages/searches/show');
   console.log('You are in the search page!');
 
@@ -38,14 +38,14 @@ server.get('/search',(req,res)=>{
 
 
 
-server.get('/new',(req,res)=>{
+server.get('/new', (req, res) => {
   res.render('./pages/new');
   console.log('You are in the search page!');
 
 });
 
 
-server.post('/searches',(req,res)=>{
+server.post('/searches', (req, res) => {
   let keyword = req.body.keyword;
   let search_by = req.body.search_by;
   let url = `https://www.googleapis.com/books/v1/volumes?q=+${search_by}:${keyword}`;
@@ -53,20 +53,11 @@ server.post('/searches',(req,res)=>{
     .then(booksdata => {
       let result = booksdata.body.items.slice(0, 10);
       let formattedResutl = result.map(bookData => new Book(bookData));
-      res.render('pages/searches/show', {books: formattedResutl});
+      res.render('pages/searches/show', { books: formattedResutl });
     });
 
 });
 
-
-
-
-
-
-server.get('*',(req,res)=>{
-
-  console.log('Sorry,something wrong');
-});
 
 
 function Book(data) {
@@ -74,5 +65,12 @@ function Book(data) {
   this.title = data.volumeInfo.title;
   this.auther = data.volumeInfo.authors ? data.volumeInfo.authors.join(', ') : 'Anonymous';
   this.description = data.volumeInfo.title;
+  // this.image = data.volumeInfo.imageLinks.thumbnail || data.volumeInfo.imageLinks.smallThumbnail || `https://i.imgur.com/J5LVHEL.jpg`;;
 }
+
+
+
+server.get('*', (req, res) => {
+  res.send('Sorry,something wrong');
+});
 
