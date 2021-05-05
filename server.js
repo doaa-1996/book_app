e5117ac861c5699981b5cfa62063055e640aeb89
doaa-1996 <doaaobeidat1996@gmail.com>
2021-05-05 22:33:05 +0300

    lab13

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = express();
 const pg = require('pg');
 require('dotenv').config();
 const cors = require('cors');
+const methodOverride = require('method-override');
 const superagent = require('superagent');
 server.use(cors());
 server.set('view engine', 'ejs');
@@ -11,7 +12,7 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.static('./public'));
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 5000;
-
+server.use(methodOverride('_method'));
 
 
 client
@@ -75,6 +76,26 @@ server.get('/books/:id', (req, res) => {
   client.query(SQL, safeValues)
     .then(data => {
       res.render('pages/books/detail', { single: data.rows[0] });
+    });
+});
+
+server.put('/updateBook/:id', (req, res) => {
+  let { image_url, title, author, description, ISBN } = req.body;
+  let SQL = `UPDATE books SET title=$1, author=$2, description=$3, image_url=$4, ISBN=$5 WHERE id=$6;`
+  let safeValues = [title, author, description, image_url, ISBN, req.params.id];
+  client.query(SQL, safeValues)
+    .then(() => {
+      res.redirect(`/books/${req.params.id}`);
+    });
+});
+
+server.delete('/deleteBook/:id', (req, res) => {
+
+  let SQL = `DELETE FROM books WHERE id=$1;`;
+  let safeValues = [req.params.id];
+  client.query(SQL, safeValues)
+    .then(() => {
+      res.redirect('/');
     });
 
 });
